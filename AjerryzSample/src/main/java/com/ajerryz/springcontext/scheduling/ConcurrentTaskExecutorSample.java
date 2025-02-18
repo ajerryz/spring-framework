d153fb3a2aab38d9aa21f6057bcd0a3e209e89d5
zhangning <ajerry@163.com>
2025-02-19 02:42:14 +0800

    [spring-context] threadpoll

---
+++ AjerryzSample/src/main/java/com/ajerryz/springcontext/scheduling/ConcurrentTaskExecutorSample.java
@@ -0,0 +1,14 @@
+package com.ajerryz.springcontext.scheduling;
+
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
+
+/**
+ * @author jerry zhang
+ * @since 2025/2/19 02:32
+ */
+public class ConcurrentTaskExecutorSample {
+
+	public static void main(String[] args) {
+		ConcurrentTaskExecutor threadPool = new ConcurrentTaskExecutor();
+	}
+}
---
+++ AjerryzSample/src/main/java/com/ajerryz/springcontext/scheduling/ThreadPoolTaskExecutorSample.java
@@ -0,0 +1,14 @@
+package com.ajerryz.springcontext.scheduling;
+
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * @author jerry zhang
+ * @since 2025/2/19 02:34
+ */
+public class ThreadPoolTaskExecutorSample {
+	public static void main(String[] args) {
+		ThreadPoolTaskExecutor threadPool = new ThreadPoolTaskExecutor();
+
+	}
+}
